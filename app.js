72e331fb7ce5ade06dd061dbe5ea029451208f8a
jayashz <jayzstha15@gmail.com>
2023-08-16 11:12:47 +0545

    added loading screen to every page refresh

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const api_url = 'https://api.chucknorris.io/jokes/random';
 const jokelist= document.getElementById('list');
 
+const loading=document.getElementById('center');
 
-getjoke();
+window.onload=()=>{
+    loading.style.display='flex';
+    getjoke();
+}
 async function getjoke(){
+    
+    loading.style.display='flex';
     const list= document.createElement('li');
     let jokes = await joke(api_url);
     list.textContent=jokes;
     jokelist.appendChild(list);
-
 }
 
 async function joke(url){
     const response= await fetch(url);
-    var theJoke= await response.json();
-    return theJoke.value;
-
+    if(response.status==200){
+        
+        loading.style.display='none';
+        var theJoke= await response.json();
+        return theJoke.value;
+    }
 }
